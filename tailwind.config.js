882ad752575256ee105d09eec47cdcdbdd71cd2f
zachkerner <zachkornerGIH@gmail.com>
2024-01-04 17:09:25 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         midnightblue: "#0a0440", 
         grouparooblue: "#29abe2", 
         airbytepurple: "#9579fd",
+        maroon: "#800000",
+        amanranth: "#B22649",
+        pinklavender: "#D5AFC3",
+        chinarose: "#AD4F74",
         link: {
           normal: "#4c74b9",
           decoration: "#d2dcee",
